4e3b8a26d9886c9da359464c21f806b3160cbfe8
Bryan O. Román Carril <bryano_dev@outlook.com>
2026-08-29 19:21:49 -0400

    jobs-table

---
+++ jobs-table/client.js
@@ -0,0 +1,19 @@
+const ICON = 'https://cdn.jsdelivr.net/npm/@mdi/v7.2.96/svg/table.svg';
+
+    window.TrelloPowerUp.initialize({
+
+      'card-back-section': function (t, options) {
+
+        return {
+          title: 'Items',
+          icon: ICON,
+
+          content: {
+            type: 'iframe',
+            url: t.signUrl('./table.html'),
+            height: 400
+          }
+        };
+      }
+
+    });
